0f12e0fadb21998f6ea7e578049665b169064be0
Jeferson Almeida <jeffalmd3@gmail.com>
2017-09-04 12:25:21 -0300

    Alteração 02-09-2017

---
+++ chamados/application/views/alteracao/buscar_usuario.php
@@ -0,0 +1,16 @@
+<?php $this->load->view('./cabecalho'); ?>
+<h4 class="card-header">Buscar por</h4>
+<div class="card-body">
+    <form action="<?php echo site_url('buscar_usuario'); ?>" method="post">
+        <div class="form-group">
+            <label for="nome" class="form-control-label">
+                Nome do usuário
+            </label>
+            <input type="text" class="form-control" id="nome" name="nome">
+        </div>
+        <div class="controls">
+            <button type="submit" class="btn btn-primary">Buscar</button>
+        </div>
+    </form>
+</div>
+<?php $this->load->view('./rodape'); ?>
