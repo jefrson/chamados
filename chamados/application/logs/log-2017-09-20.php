0907377ae4f98850030e794d46c7ac5b4f39ac4f
Jeferson Almeida <jeffalmd3@gmail.com>
2017-10-05 21:25:29 -0300

    Revert "Alteração 03-10-2017"
    This reverts commit e472f2c19761bd397f2fbfe8e5d1771f8e076a63.

--- chamados/application/logs/log-2017-09-20.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-09-20 14:09:06 --> Severity: error --> Exception: Call to undefined method CI_DB_mysqli_driver::num_rows() C:\xampp\htdocs\chamados\application\models\andamento_model.php 45
-ERROR - 2017-09-20 18:20:00 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Nenhuma conex�o p�de ser feita porque a m�quina de destino as recusou ativamente.
- C:\xampp\htdocs\chamados\system\database\drivers\mysqli\mysqli_driver.php 201
-ERROR - 2017-09-20 18:20:00 --> Unable to connect to the database
-ERROR - 2017-09-20 18:20:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Nenhuma conex�o p�de ser feita porque a m�quina de destino as recusou ativamente.
- C:\xampp\htdocs\chamados\system\database\drivers\mysqli\mysqli_driver.php 201
-ERROR - 2017-09-20 18:20:14 --> Unable to connect to the database
